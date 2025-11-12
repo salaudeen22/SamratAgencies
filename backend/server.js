7b5253f1009b81455d857452b61cd150df6fe8e7
salaudeen22 <salaudeen22@users.noreply.github.com>
2025-11-12 10:34:43 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: [process.env.FRONTEND_URL, 'https://samratagencies.netlify.app'],
   credentials: true
 }));
 app.use(express.json());
